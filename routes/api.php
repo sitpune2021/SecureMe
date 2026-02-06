4f8ca9d6f3f986009e36112583f7a21a430c8c6b
mayurkak <mayur077kakde@gmail.com>
2026-02-06 11:38:38 +0530

    apply any method to logout

--- routes/api.php
+++ routes/api.php
@@ -18,22 +18,11 @@ Route::prefix('auth')->group(function () {
 
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
-
-
     Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/profile', [AuthController::class, 'profile']);
-        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::any('/logout', [AuthController::class, 'logout']);
     });
 });
-
-
-
-
-
-
-
-
-
 
 Route::middleware('auth:sanctum')->group(function () {
 
